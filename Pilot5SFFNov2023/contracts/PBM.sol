1e5d9b2bd7d8beaabd8bf1ec59386c9a5b8e42a2
Jacob Shan <jacobshan429@gmail.com>
2023-05-30 01:55:24 +0800

    chore: simplify envelope logic and fix issuer integration tests

---
+++ Pilot5SFFNov2023/contracts/PBM.sol
@@ -0,0 +1,305 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+import "./ERC20Helper.sol";
+import "./PBMTokenManager.sol";
+import "./IPBM.sol";
+import "./IPBMAddressList.sol";
+
+contract PBM is ERC1155, Ownable, Pausable, IPBM {
+    // undelrying ERC-20 tokens
+    address public spotToken = address(0);
+    // address of the token manager
+    address public pbmTokenManager = address(0);
+    // address of the PBM-Addresslist
+    address public pbmAddressList = address(0);
+
+    // tracks contract initialisation
+    bool internal initialised = false;
+    // time of expiry ( epoch )
+    uint256 public contractExpiry;
+
+    constructor() ERC1155("") {
+        pbmTokenManager = address(new PBMTokenManager());
+    }
+    //mapping to keep track of how much an user has loaded to PBM
+    mapping(address => uint256) public userWalletBalance;
+
+    function initialise(address _spotToken, uint256 _expiry, address _pbmAddressList) external override onlyOwner {
+        require(!initialised, "PBM: Already initialised");
+        require(Address.isContract(_spotToken), "Invalid spot token");
+        require(Address.isContract(_pbmAddressList), "Invalid spot token");
+        spotToken = _spotToken;
+        contractExpiry = _expiry;
+        pbmAddressList = _pbmAddressList;
+
+        initialised = true;
+    }
+
+    /**
+     * @dev See {IPBM-createPBMTokenType}.
+     *
+     * Requirements:
+     *
+     * - caller must be owner
+     * - contract must not be expired
+     * - `tokenExpiry` must be less than contract expiry
+     * - `amount` should not be 0
+     */
+    function createPBMTokenType(
+        string memory companyName,
+        uint256 spotAmount,
+        uint256 tokenExpiry,
+        address creator,
+        string memory tokenURI,
+        string memory postExpiryURI
+    ) external override onlyOwner {
+        PBMTokenManager(pbmTokenManager).createTokenType(
+            companyName,
+            spotAmount,
+            tokenExpiry,
+            creator,
+            tokenURI,
+            postExpiryURI,
+            contractExpiry
+        );
+    }
+
+    /**
+     * @dev See {IPBM-mint}.
+     *     
+     * IMPT: Before minting, the caller should approve the contract address to spend ERC-20 tokens on behalf of the caller.
+     *       This can be done by calling the `approve` or `increaseMinterAllowance` functions of the ERC-20 contract and specifying `_spender` to be the PBM contract address. 
+             Ref : https://eips.ethereum.org/EIPS/eip-20
+
+       WARNING: Any contracts that externally call these mint() and batchMint() functions should implement some sort of reentrancy guard procedure (such as OpenZeppelin's ReentrancyGuard).
+     *
+     * Requirements:
+     *
+     * - contract must not be paused
+     * - tokens must not be expired
+     * - `tokenId` should be a valid id that has already been created
+     * - caller should have the necessary amount of the ERC-20 tokens required to mint
+     * - caller should have approved the PBM contract to spend the ERC-20 tokens
+     * - receiver should not be blacklisted
+     */
+    function mint(uint256 tokenId, uint256 amount, address receiver) external override whenNotPaused {
+        require(!IPBMAddressList(pbmAddressList).isBlacklisted(receiver), "PBM: 'to' address blacklisted");
+
+        // mint the token if the contract - wrapping the xsgd
+        PBMTokenManager(pbmTokenManager).increaseBalanceSupply(serialise(tokenId), serialise(amount));
+        _mint(receiver, tokenId, amount, "");
+    }
+
+    /**
+     * @dev See {IPBM-batchMint}.
+     *
+
+       WARNING: Any contracts that externally call these mint() and batchMint() functions should implement some sort of reentrancy guard procedure (such as OpenZeppelin's ReentrancyGuard).
+     *
+     * Requirements:
+     *
+     * - contract must not be paused
+     * - tokens must not be expired
+     * - `tokenIds` should all be valid ids that have already been created
+     * - `tokenIds` and `amounts` list need to have the same number of values
+     * - caller should have the necessary amount of the ERC-20 tokens required to mint
+     * - caller should have approved the PBM contract to spend the ERC-20 tokens
+     * - receiver should not be blacklisted
+     */
+    function batchMint(
+        uint256[] memory tokenIds,
+        uint256[] memory amounts,
+        address receiver
+    ) external override whenNotPaused {
+        require(!IPBMAddressList(pbmAddressList).isBlacklisted(receiver), "PBM: 'to' address blacklisted");
+        require(tokenIds.length == amounts.length, "Unequal ids and amounts supplied");
+
+        PBMTokenManager(pbmTokenManager).increaseBalanceSupply(tokenIds, amounts);
+        _mintBatch(receiver, tokenIds, amounts, "");
+    }
+
+    /**
+     * @dev See {IPBM-load}.
+     *
+     * IMPT: Before loading, the caller should approve the contract address to spend ERC-20 tokens on behalf of the caller.
+     *       This can be done by calling the `approve` or `increaseMinterAllowance` functions of the ERC-20 contract and specifying `_spender` to be the PBM contract address.
+             Ref : https://eips.ethereum.org/EIPS/eip-20
+     *
+     * Requirements:
+     *
+     * - contract must not be paused
+     * - tokens must not be expired
+     * - `tokenId` should all be valid ids that have already been created
+     * - caller should have the necessary amount of PBM envelope tokens required to load spot
+     * - caller should have the necessary amount of the ERC-20 tokens required to load
+     * - caller should have approved the PBM contract to spend the ERC-20 tokens
+     */
+
+    function load(address originalCaller,uint256 tokenId, uint256 spotAmount) external whenNotPaused{
+        require(balanceOf(originalCaller, tokenId) >= 1, "PBM: Don't have enough PBM envelope to load spot");
+        ERC20Helper.safeTransferFrom(spotToken, msg.sender, address(this), spotAmount);
+        userWalletBalance[originalCaller] += spotAmount;
+        // other loading logics can also be added here.
+    }
+
+    /**
+     * @dev See {IPBM-unLoad}.
+     *
+     *
+     * Requirements:
+     *
+     * - contract must not be paused
+     * - caller should have loaded to the PBM envelope before
+     */
+
+    function unLoad(address originalCaller, uint256 spotAmount) external whenNotPaused{
+        require(userWalletBalance[originalCaller] > 0, "PBM: Don't have any spot to unload");
+        ERC20Helper.safeTransferFrom(spotToken,address(this), msg.sender, spotAmount);
+        userWalletBalance[originalCaller] -= spotAmount;
+    }
+
+    /**
+     * @dev See {IPBM-safeTransferFrom}.
+     *
+     *
+     * Requirements:
+     *
+     * - contract must not be paused
+     * - tokens must not be expired
+     * - `tokenId` should be a valid ids that has already been created
+     * - caller should have the PBMs that are being transferred (or)
+     * - caller should have the approval to spend the PBMs on behalf of the owner (`from` addresss)
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override(ERC1155, IPBM) whenNotPaused {
+        require(
+            from == _msgSender() || isApprovedForAll(from, _msgSender()),
+            "ERC1155: caller is not token owner nor approved"
+        );
+        require(!IPBMAddressList(pbmAddressList).isBlacklisted(to), "PBM: 'to' address blacklisted");
+
+        if (IPBMAddressList(pbmAddressList).isMerchant(to)) {
+            // when call safeTransferFrom on a envelope PBM need to encode the payment amount into the data field
+            uint spotAmount = abi.decode(data, (uint256));
+            require(userWalletBalance[from] >= spotAmount, "PBM: Don't have enough spot to pay");
+            userWalletBalance[from] -= spotAmount;
+            ERC20Helper.safeTransfer(spotToken, to, spotAmount);
+            emit MerchantPayment(from, to, serialise(id), serialise(amount), spotToken, spotAmount);
+        }
+        _safeTransferFrom(from, to, id, amount, data);
+
+    }
+
+    /**
+     * @dev See {IPBM-safeBatchTransferFrom}.
+     *
+     *
+     * Requirements:
+     *
+     * - contract must not be paused
+     * - tokens must not be expired
+     * - `tokenIds` should all be  valid ids that has already been created
+     * - `tokenIds` and `amounts` list need to have the same number of values
+     * - caller should have the PBMs that are being transferred (or)
+     * - caller should have the approval to spend the PBMs on behalf of the owner (`from` addresss)
+     */
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override(ERC1155, IPBM) whenNotPaused {
+        require(
+            from == _msgSender() || isApprovedForAll(from, _msgSender()),
+            "ERC1155: caller is not token owner nor approved"
+        );
+        require(!IPBMAddressList(pbmAddressList).isBlacklisted(to), "PBM: 'to' address blacklisted");
+        require(ids.length == amounts.length, "Unequal ids and amounts supplied");
+
+        if (IPBMAddressList(pbmAddressList).isMerchant(to)) {
+            // when call safeTransferFrom on a envelope PBM need to encode the payment amount into the data field
+            uint spotAmount = abi.decode(data, (uint256));
+            require(userWalletBalance[from] >= spotAmount, "PBM: Don't have enough spot to pay");
+            userWalletBalance[from] -= spotAmount;
+            ERC20Helper.safeTransfer(spotToken, to, spotAmount);
+            emit MerchantPayment(from, to, ids, amounts, spotToken, spotAmount);
+        }
+        _safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    /**
+     * @dev See {IPBM-revokePBM}.
+     *
+     * Requirements:
+     *
+     * - `tokenId` should be a valid ids that has already been created
+     * - caller must be the creator of the tokenType
+     * - token must be expired
+     */
+    function revokePBM(uint256 tokenId) external override whenNotPaused {
+        uint256 valueOfTokens = PBMTokenManager(pbmTokenManager).getPBMRevokeValue(tokenId);
+
+        PBMTokenManager(pbmTokenManager).revokePBM(tokenId, msg.sender);
+
+        // transfering underlying ERC20 tokens
+        ERC20Helper.safeTransfer(spotToken, msg.sender, valueOfTokens);
+
+        emit PBMrevokeWithdraw(msg.sender, tokenId, spotToken, valueOfTokens);
+    }
+
+    /**
+     * @dev See {IPBM-getTokenDetails}.
+     *
+     */
+    function getTokenDetails(
+        uint256 tokenId
+    ) external view override returns (string memory, uint256, uint256, address) {
+        return PBMTokenManager(pbmTokenManager).getTokenDetails(tokenId);
+    }
+
+    /**
+     * @dev See {IPBM-uri}.
+     *
+     */
+    function uri(uint256 tokenId) public view override(ERC1155, IPBM) returns (string memory) {
+        return PBMTokenManager(pbmTokenManager).uri(tokenId);
+    }
+
+    /**
+     * @dev see {Pausable _pause}
+     *
+     * Requirements :
+     * - caller should be owner
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev see {Pausable _unpause}
+     *
+     * Requirements :
+     * - caller should be owner
+     */
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function serialise(uint256 num) internal pure returns (uint256[] memory) {
+        uint256[] memory array = new uint256[](1);
+        array[0] = num;
+        return array;
+    }
+}
---
+++ Pilot5SFFNov2023/test/IssuerHelper.test.js
@@ -0,0 +1,228 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { signMetaTxRequest } = require("../src/signer");
+
+async function deploy(name, ...params) {
+  const Contract = await ethers.getContractFactory(name);
+  return await Contract.deploy(...params).then(f => f.deployed());
+}
+
+describe("IssuerHelper", function () {
+  beforeEach(async function () {
+    // Get the signers
+    [deployer, whitelister, wallet, other] = await ethers.getSigners();
+
+    // Deploy a mock ERC20 token
+    this.erc20Token = await deploy("Spot");
+
+    // Deploy the PBM contract
+    this.pbm = await deploy("PBM");
+
+    // Deploy the MinimalForwarder contract
+    this.minimalForwarder = await deploy("MinimalForwarder");
+
+    // Deploy the IssuerHelper contract
+    this.issuerHelper = await deploy("IssuerHelper", this.pbm.address, this.minimalForwarder.address);
+
+    this.addressList = await deploy("PBMAddressList");
+
+    await this.issuerHelper.addWhitelister(deployer.address);
+
+    // Add the whitelister and wallet as allowed entities
+    await this.issuerHelper.addWhitelister(whitelister.address);
+    await this.issuerHelper.addWhitelistedWallet(wallet.address);
+
+    // Give the wallet some tokens
+    await this.erc20Token.mint(wallet.address, ethers.utils.parseUnits("1000", 18));
+
+    // create PBM envelope token type
+    currentDate = new Date();
+    currentEpoch = Math.floor(currentDate / 1000);
+    var targetEpoch = currentEpoch + 100000; // Expiry is set to 1 day 3.6 hours from current time
+    await this.pbm.connect(deployer).initialise(this.erc20Token.address, targetEpoch + 100000000, this.addressList.address);
+    await this.pbm.connect(deployer).createPBMTokenType("testPBMenvelope", 5, targetEpoch, deployer.address, "uri", "expired_uri");
+  });
+
+  async function relay(forwarder, request, signature, whitelist) {
+    // Decide if we want to relay this request based on a whitelist
+    const accepts = !whitelist || whitelist.includes(request.to);
+    if (!accepts) throw new Error(`Rejected request to ${request.to}`);
+
+    // Validate request on the forwarder contract
+    const valid = await forwarder.verify(request, signature);
+
+    if (!valid) throw new Error(`Invalid request`);
+
+    // Send meta-tx through relayer to the forwarder contract
+    const gasLimit = (parseInt(request.gas) * 1.5).toString();
+    return await forwarder.execute(request, signature, { gasLimit });
+  }
+
+  describe("meta txn", function () {
+    it("should call the addWhitelistedWallet thru a metatxn", async function () {
+      const { minimalForwarder, issuerHelper } = this;
+
+      const { request, signature } = await signMetaTxRequest(whitelister.provider, minimalForwarder, {
+        from: whitelister.address,
+        to: issuerHelper.address,
+        data: issuerHelper.interface.encodeFunctionData('addWhitelistedWallet', [other.address]),
+      });
+
+      const whitelist = [issuerHelper.address]
+
+      // Check whitelister's initial ether balance
+      const initialWhitelisterBalance = await ethers.provider.getBalance(whitelister.address);
+      await relay(minimalForwarder, request, signature, whitelist);
+
+      // Check whitelister's final ether balance
+      const finalWhitelisterBalance = await ethers.provider.getBalance(whitelister.address);
+      expect(await issuerHelper.whitelistedWallets(other.address)).to.equal(true);
+
+      // Compare initial and final balances to ensure that whitelister didn't pay gas
+      expect(initialWhitelisterBalance).to.equal(finalWhitelisterBalance);
+
+    })
+
+    it("should fail if caller is not an allowed whitelister", async function () {
+      const { minimalForwarder, issuerHelper } = this;
+      const { request, signature } = await signMetaTxRequest(other.provider, minimalForwarder, {
+        from: other.address,
+        to: issuerHelper.address,
+        data: issuerHelper.interface.encodeFunctionData('addWhitelistedWallet', [other.address]),
+      });
+      const whitelist = [issuerHelper.address]
+
+      // Check other's initial ether balance
+      const initialOtherBalance = await ethers.provider.getBalance(other.address);
+
+      await relay(minimalForwarder, request, signature, whitelist);
+
+      // Check other's final ether balance
+      const finalOtherBalance = await ethers.provider.getBalance(other.address);
+
+      // Compare initial and final balances to ensure that other didn't pay gas
+      expect(initialOtherBalance).to.equal(finalOtherBalance);
+
+      // based on the forwarder contract implementation 
+      // here the excute function call would finish 
+      // but the underlying addWhitelistedWallet function call of this meta txn would be reverted
+      // and the state of the issuer helper contract would remain unchanged
+      // the returndata returned by the execute function would contain the revert reason
+      // and the error message emitted by the underlying function call
+      // in order to inspect the returndata might need to emit an event in the execute function call
+      // will leave it to the forwarder contract implenmentor to figure out the details
+
+      expect(await issuerHelper.whitelistedWallets(other.address)).to.equal(false);
+    });
+
+    it("should call the processLoadAndSafeTransfer thru a metatxn", async function () {
+      const { pbm, addressList, erc20Token, issuerHelper, minimalForwarder } = this;
+
+
+      // mint envelope to wallet
+      await pbm.mint(0, 1, wallet.address)
+
+      // Grant permission to the issuerHelper to transfer the envelope token
+      await pbm.connect(wallet).setApprovalForAll(issuerHelper.address, true);
+
+      // Grant allowance to the issuerHelper contract by the wallet
+      await erc20Token.connect(wallet).approve(issuerHelper.address, ethers.utils.parseUnits("100", 18));
+
+      // add other wallet as a merchat
+      await addressList.connect(deployer).addMerchantAddresses([other.address], 'other');
+
+      // Check the initial balances
+      const initialOtherBalance = await erc20Token.balanceOf(other.address);
+      const initialWalletBalance = await erc20Token.balanceOf(wallet.address);
+
+      // Perform the processLoadAndSafeTransferFrom metatxn call
+      const { request, signature } = await signMetaTxRequest(wallet.provider, minimalForwarder, {
+        from: wallet.address,
+        to: issuerHelper.address,
+        data: issuerHelper.interface.encodeFunctionData('processLoadAndSafeTransferFrom', [erc20Token.address, wallet.address, other.address, 0, ethers.utils.parseUnits("50", 18)]),
+      });
+
+      const whitelist = [issuerHelper.address]
+
+      // Check wallet's initial ether balance
+      const initialWalletEthBalance = await ethers.provider.getBalance(wallet.address);
+
+      await relay(minimalForwarder, request, signature, whitelist);
+
+      // Check wallet's final ether balance
+      const finalWalletEthBalance = await ethers.provider.getBalance(wallet.address);
+
+      // Compare initial and final balances to ensure that other didn't pay gas
+      expect(initialWalletEthBalance).to.equal(finalWalletEthBalance);
+
+      // Check the final balances
+      const finalOtherBalance = await erc20Token.balanceOf(other.address);
+      const finalWalletBalance = await erc20Token.balanceOf(wallet.address);
+      expect(finalOtherBalance.sub(initialOtherBalance)).to.equal(ethers.utils.parseUnits("50", 18));
+      expect(initialWalletBalance.sub(finalWalletBalance)).to.equal(ethers.utils.parseUnits("50", 18));
+      expect(await pbm.balanceOf(other.address, 0) == 1).to.equal(true);
+    })
+
+  })
+
+  describe("processLoadAndSafeTransfer", function () {
+    it("should transfer tokens from wallet to IssuerHelper and call loadAndSafeTransfer on PBM", async function () {
+      const { pbm, addressList, erc20Token, issuerHelper } = this;
+      // Grant allowance to the issuerHelper contract by the wallet
+      await erc20Token.connect(wallet).approve(issuerHelper.address, ethers.utils.parseUnits("100", 18));
+      // mint envelope to wallet
+      await pbm.mint(0, 1, wallet.address)
+
+      // Grant permission to the issuerHelper to transfer the envelope token
+      await pbm.connect(wallet).setApprovalForAll(issuerHelper.address, true);
+
+
+      // add other wallet as a merchat
+      await addressList.connect(deployer).addMerchantAddresses([other.address], 'other');
+
+      // Check the initial balances
+      const initialOtherBalance = await erc20Token.balanceOf(other.address);
+      const initialWalletBalance = await erc20Token.balanceOf(wallet.address);
+
+      // Check wallet's initial ether balance
+      const initialWalletEthBalance = await ethers.provider.getBalance(wallet.address);
+      // Perform the processLoadAndSafeTransfer function call
+      txn = await issuerHelper.connect(wallet).processLoadAndSafeTransferFrom(erc20Token.address, wallet.address, other.address, 0, ethers.utils.parseUnits("50", 18));
+      receipt = await txn.wait();
+
+      // Calculate the gas used and cost
+      const gasUsed = receipt.gasUsed;
+      const gasPrice = txn.gasPrice;
+      const gasCost = gasUsed.mul(gasPrice);
+
+      // Check wallet's final ether balance
+      const finalWalletEthBalance = await ethers.provider.getBalance(wallet.address);
+
+      // Check if the wallet's Ether balance changes equal to the gas used for the transaction
+      expect(initialWalletEthBalance.sub(finalWalletEthBalance)).to.equal(gasCost);
+
+      // Check the final balances
+      const finalOtherBalance = await erc20Token.balanceOf(other.address);
+      const finalWalletBalance = await erc20Token.balanceOf(wallet.address);
+      expect(finalOtherBalance.sub(initialOtherBalance)).to.equal(ethers.utils.parseUnits("50", 18));
+      expect(initialWalletBalance.sub(finalWalletBalance)).to.equal(ethers.utils.parseUnits("50", 18));
+      expect(await pbm.balanceOf(other.address, 0) == 1).to.equal(true);
+    });
+
+    it("should fail if caller is not an allowed whitelister", async function () {
+      const { issuerHelper } = this;
+      await expect(
+        issuerHelper.connect(other).addWhitelistedWallet(wallet.address)
+      ).to.be.revertedWith("Caller is not an allowed whitelister");
+    });
+
+    it("should fail if wallet is not whitelisted", async function () {
+      const { issuerHelper, erc20Token } = this;
+      await issuerHelper.removeWhitelistedWallet(wallet.address);
+      await expect(
+        issuerHelper.connect(whitelister).processLoadAndSafeTransferFrom(erc20Token.address, wallet.address, other.address, 0, ethers.utils.parseUnits("50", 18))
+      ).to.be.revertedWith("Wallet is not whitelisted");
+    });
+  });
+});
+
