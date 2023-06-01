6c7a9dbe0fa11d59954d99ac9e65ca4736895ac0
Jacob Shan <jacobshan429@gmail.com>
2023-06-01 10:59:04 +0800

    chore: added minting script

--- Pilot5SFFNov2023/migrations/2_initialise_pbm.js
+++ Pilot5SFFNov2023/migrations/2_initialise_pbm.js
@@ -1,18 +1,20 @@
-module.exports = async ({getNamedAccounts, deployments, ethers}) => {
-    const {deployer} = await getNamedAccounts();
-    const deployerSigner = ethers.provider.getSigner(deployer);
+module.exports = async ({ getNamedAccounts, deployments, ethers }) => {
+  const { deployer } = await getNamedAccounts();
+  const deployerSigner = ethers.provider.getSigner(deployer);
 
-    const pbmDeployment = await deployments.get('PBM');
-    const pbm = (await ethers.getContractFactory('PBM')).attach(pbmDeployment.address).connect(deployerSigner);
-    const addressListDeployment = await deployments.get('PBMAddressList');
+  const pbmDeployment = await deployments.get('PBM');
+  const pbm = (await ethers.getContractFactory('PBM'))
+    .attach(pbmDeployment.address)
+    .connect(deployerSigner);
+  const addressListDeployment = await deployments.get('PBMAddressList');
 
-    // const currentDate = new Date();
-    // const currentEpoch = Math.floor(currentDate / 1000);
-    const expiryDate = 1716469200; // Tue May 23 2024 21:00:00 GMT+0800 (Taipei Standard Time) 2024-05-23T21:00:00+08:00
+  // const currentDate = new Date();
+  // const currentEpoch = Math.floor(currentDate / 1000);
+  const expiryDate = 1716469200; // Tue May 23 2024 21:00:00 GMT+0800 (Taipei Standard Time) 2024-05-23T21:00:00+08:00
 
-    // Polygon XSGD address = "0xDC3326e71D45186F113a2F448984CA0e8D201995"
-    // Mumbai XSGD address = "0x16e28369bc318636abbf6cb1035da77ffbf4a3bc"
-    const address = "0x16e28369bc318636abbf6cb1035da77ffbf4a3bc"
-    await pbm.initialise(address, expiryDate, addressListDeployment.address);
-    console.log("PBM initialised")
-}
+  // Polygon XSGD address = "0xDC3326e71D45186F113a2F448984CA0e8D201995"
+  // Mumbai XSGD address = "0x16e28369bc318636abbf6cb1035da77ffbf4a3bc"
+  const address = '0x16e28369bc318636abbf6cb1035da77ffbf4a3bc';
+  await pbm.initialise(address, expiryDate, addressListDeployment.address);
+  console.log('PBM initialised');
+};
