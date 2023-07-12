b0ad1162f7f8a439d1aabf015b90d451177e49b5
Jacob Shan <jacobshan429@gmail.com>
2023-07-13 00:15:27 +0800

    chore: added mint script

--- heroNFT2023SEP/scripts/deploy.js
+++ heroNFT2023SEP/scripts/deploy.js
@@ -6,9 +6,31 @@
 // global scope, and execute the script.
 const hre = require('hardhat');
 
+module.exports = main;
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log('Deploying contracts with the account:', deployer.address);
+
   const heroNFT = await hre.ethers.deployContract('HeroNFT');
+
   console.log(`Deployed HeroNFT to ${heroNFT.address}`);
+
+  const tokenId = 0; // Define the token ID
+  const amount = 1; // Define the amount
+  const data = '0x'; // Define additional data here if needed
+
+  // Call the mint function to mint to deployer address
+  const mintTx = await heroNFT.mint(deployer.address, tokenId, amount, data);
+  // Wait for it to be mined
+  await mintTx.wait();
+
+  console.log(`Minted token ${tokenId} to ${deployer.address}`);
 }
 
-module.exports = main;
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
