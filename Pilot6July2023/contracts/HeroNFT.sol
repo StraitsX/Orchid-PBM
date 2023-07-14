39c38ffd7263d905b57893d667ecc7af5a9298a3
Jacob Shan <jacobshan429@gmail.com>
2023-07-15 06:11:43 +0800

    feat(heroNFT): airdrop heroNFT upon first successful payment

---
+++ Pilot6July2023/contracts/HeroNFT.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract HeroNFT is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
+    constructor() ERC1155("") {}
+
+    // better open a dedicated repo on github to store the metadata
+    string baseURI = "https://raw.githubusercontent.com/StraitsX/NFT/main/heroNFT2023SEP/";
+    mapping(address => bool) public whitelisted;
+
+    function setURI(string memory baseUri) public onlyOwner {
+        baseURI = baseUri;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function uri(uint256 token_id) public view override returns (string memory) {
+        return string(abi.encodePacked(baseURI, Strings.toString(token_id), ".json"));
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyWhitelisted {
+        require(amount == 1, "HeroNFT: Amount must be 1");
+        require(balanceOf(account, id) == 0, "HeroNFT: Account already owns this token_id");
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyWhitelisted {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) whenNotPaused {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    modifier onlyWhitelisted() {
+        require(whitelisted[msg.sender] || msg.sender == owner(), "Caller is not an whitelisted address nor the owner");
+        _;
+    }
+
+    function addWhitelisted(address _whitelistAddress) external onlyOwner {
+        whitelisted[_whitelistAddress] = true;
+    }
+
+    function removeWhitelisted(address _whitelistAddress) external onlyOwner {
+        whitelisted[_whitelistAddress] = false;
+    }
+}
---
+++ Pilot6July2023/test/heroNFTTransfer.test.js
@@ -0,0 +1,117 @@
+const { expect } = require('chai');
+const {
+  deploy,
+  getSigners,
+  createTokenType,
+  parseUnits,
+  mintPBM,
+  whilteListMerchant,
+  addMerchantAsHero,
+} = require('./testHelper.js');
+
+// Main Test script
+describe('PBM', () => {
+  let accounts;
+  let owner, merchant1, merchant2, merchant3, nonMerchant;
+
+  before(async () => {
+    accounts = await getSigners();
+    [owner, merchant1, merchant2, merchant3, nonMerchant] = accounts;
+  });
+
+  describe('PBM transfer test', () => {
+    let spot, pbm, addressList, heroNFT;
+
+    beforeEach(async () => {
+      spot = await deploy('Spot');
+      pbm = await deploy('PBM');
+      addressList = await deploy('PBMAddressList');
+      heroNFT = await deploy('HeroNFT');
+      await pbm.initialise(
+        spot.address,
+        Math.round(new Date().getTime() / 1000 + 86400 * 30),
+        addressList.address,
+        heroNFT.address,
+      );
+
+      // whiteList pbm as heroNFT minter
+      await heroNFT.addWhitelisted(pbm.address);
+      expect(await heroNFT.whitelisted(pbm.address)).to.be.equal(true);
+
+      await spot.mint(owner.address, parseUnits('10000', 6));
+      await createTokenType(pbm, '1XSGD', '1', owner);
+      await whilteListMerchant(addressList, [
+        merchant1.address,
+        merchant2.address,
+        merchant3.address,
+      ]);
+      await addMerchantAsHero(
+        addressList,
+        [merchant1.address, merchant2.address, merchant3.address],
+        [1, 2, 3],
+      );
+      await mintPBM(pbm, spot, 0, 1, owner.address, '1');
+    });
+
+    it('transfer to hero merchant successfully mint heroNFT to user', async () => {
+      // Perform transfer
+      await pbm.safeTransferFrom(owner.address, merchant1.address, 0, 1, '0x');
+      // Check balances after transfer
+      const heroMerchantAfterSpotBalance = await spot.balanceOf(
+        merchant1.address,
+      );
+      expect(heroMerchantAfterSpotBalance).to.be.equal(parseUnits('1', 6));
+      const ownerHeroNFTBalance = await heroNFT.balanceOf(owner.address, 1);
+      expect(ownerHeroNFTBalance).to.be.equal(1);
+    });
+
+    it('transfer to hero merchant successfully will not mint heroNFT if user already have one', async () => {
+      // Perform transfer to hero merchant 2
+      await pbm.safeTransferFrom(owner.address, merchant2.address, 0, 1, '0x');
+      // Check balances after transfer
+      const heroMerchantAfterSpotBalance = await spot.balanceOf(
+        merchant2.address,
+      );
+      expect(heroMerchantAfterSpotBalance).to.be.equal(parseUnits('1', 6));
+      // expect to get heroNFT id 2
+      const ownerHeroNFTBalance = await heroNFT.balanceOf(owner.address, 2);
+      expect(ownerHeroNFTBalance).to.be.equal(1);
+
+      // mint another PBM
+      await mintPBM(pbm, spot, 0, 1, owner.address, '1');
+      // Perform transfer to hero merchant 2 again won't receive another heroNFT
+      await pbm.safeTransferFrom(owner.address, merchant2.address, 0, 1, '0x');
+
+      const heroMerchantFinalSpotBalance = await spot.balanceOf(
+        merchant2.address,
+      );
+      expect(heroMerchantFinalSpotBalance).to.be.equal(parseUnits('2', 6));
+
+      const ownerFinalHeroNFTBalance = await heroNFT.balanceOf(
+        owner.address,
+        2,
+      );
+      expect(ownerFinalHeroNFTBalance).to.be.equal(1);
+    });
+
+    it('batch Transfer to hero merchant successfully mint heroNFT to user', async () => {
+      await createTokenType(pbm, '2XSGD', '2', owner);
+      await mintPBM(pbm, spot, 1, 1, owner.address, '2');
+      // Perform transfer
+      await pbm.safeBatchTransferFrom(
+        owner.address,
+        merchant1.address,
+        [0, 1],
+        [1, 1],
+        '0x',
+      );
+      // Check balances after transfer
+      const heroMerchantAfterSpotBalance = await spot.balanceOf(
+        merchant1.address,
+      );
+      expect(heroMerchantAfterSpotBalance).to.be.equal(parseUnits('3', 6));
+      const ownerHeroNFTBalance = await heroNFT.balanceOf(owner.address, 1);
+      expect(ownerHeroNFTBalance).to.be.equal(1);
+    });
+  });
+});
