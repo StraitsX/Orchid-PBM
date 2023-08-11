b451256ef31385c5c9a5a79b1e3fd146d8c04308
Jacob Shan <jacobshan429@gmail.com>
2023-08-11 18:52:48 +0800

    chore: update scripts and deploy on mumbai

--- Pilot6July2023/scripts/create_token_type.js
+++ Pilot6July2023/scripts/create_token_type.js
@@ -4,28 +4,41 @@ async function main() {
   const deployerSigner = ethers.provider.getSigner(deployer);
 
   const pbmDeployment = await deployments.get('PBM');
-  const pbm = (await ethers.getContractFactory('PBM')).attach(pbmDeployment.address).connect(deployerSigner);
+  const pbm = (await ethers.getContractFactory('PBM'))
+    .attach(pbmDeployment.address)
+    .connect(deployerSigner);
 
-  const xsgdAddress = "0x787bD10Bb65AE206f70759D88a2ffc0F2653C0F6"
-  const dsgdAddress = "0xd769410dc8772695A7f55a304d2125320A65c2a5"
+  // polygon mainnet fake stable coins addresses
+  // const xsgdAddress = '0x787bD10Bb65AE206f70759D88a2ffc0F2653C0F6';
+  // const dsgdAddress = '0xd769410dc8772695A7f55a304d2125320A65c2a5';
 
+
+  // mumbai xsgd and 2 decimals dsgd addresses
+  const xsgdAddress = '0x16e28369bc318636abbf6cb1035da77ffbf4a3bc';
+  const dsgdAddress = '0xD2a6a1F2954a04EF90152da0fC9E15b5c68E7A69';
 
   const expiryDate = 1716469200; // Tue May 23 2024 21:00:00 GMT+0800 (Taipei Standard Time) 2024-05-23T21:00:00+08:00
 
   // mint 1000000 $SGD worth of DSGD to deployer
-  const dsgdAmount = ethers.utils.parseUnits('1000000', 6);
-  const dsgd = (await ethers.getContractFactory('Spot')).attach(dsgdAddress).connect(deployerSigner);
+  const dsgd = (await ethers.getContractFactory('Spot'))
+    .attach(dsgdAddress)
+    .connect(deployerSigner);
+  const dsgdAmount = ethers.utils.parseUnits('1000000', await dsgd.decimals());
+
   await dsgd.mint(deployer, dsgdAmount);
 
   // mint 1000000 $SGD worth of XSGD to deployer
-  const xsgdAmount = ethers.utils.parseUnits('1000000', 6);
-  const xsgd = (await ethers.getContractFactory('Spot')).attach(xsgdAddress).connect(deployerSigner);
+  const xsgd = (await ethers.getContractFactory('Spot'))
+    .attach(xsgdAddress)
+    .connect(deployerSigner);
+  const xsgdAmount = ethers.utils.parseUnits('1000000', await xsgd.decimals());
+
   await xsgd.mint(deployer, xsgdAmount);
 
   // creating token id 0: XSGD pbm - 1 XSGD
   await pbm.createPBMTokenType(
     'Grab1XSGD',
-    ethers.utils.parseUnits('1', 6),
+    ethers.utils.parseUnits('1', await xsgd.decimals()),
     'XSGD',
     expiryDate,
     deployer,
@@ -38,7 +51,7 @@ async function main() {
   // creating token id 1: XSGD pbm - 2 XSGD
   await pbm.createPBMTokenType(
     'Grab2XSGD',
-    ethers.utils.parseUnits('2', 6),
+    ethers.utils.parseUnits('2', await xsgd.decimals()),
     'XSGD',
     expiryDate,
     deployer,
@@ -51,7 +64,7 @@ async function main() {
   // creating token id 2: XSGD pbm - 5 XSGD
   await pbm.createPBMTokenType(
     'Grab5XSGD',
-    ethers.utils.parseUnits('5', 6),
+    ethers.utils.parseUnits('5', await xsgd.decimals()),
     'XSGD',
     expiryDate,
     deployer,
@@ -61,17 +74,34 @@ async function main() {
   console.log('PBM Token type 2 created');
   await new Promise((r) => setTimeout(r, 5000)); // UNCOMMENT to prevent rpc rate limiting if you are on free version
 
-  // creating token id 3: DSGD pbm - 1 DSGD
+
+  // https://raw.githubusercontent.com/StraitsX/NFT-Metadata/main/pilot6July2023/0.1XSGD.json
+
+  // creating token id 3: XSGD pbm - 0.1 XSGD
+  await pbm.createPBMTokenType(
+      '0.1XSGD',
+      ethers.utils.parseUnits('0.1', await xsgd.decimals()),
+      'XSGD',
+      expiryDate,
+      deployer,
+      'https://raw.githubusercontent.com/StraitsX/NFT-Metadata/main/pilot6July2023/0.1XSGD.json',
+      'https://raw.githubusercontent.com/StraitsX/NFT-Metadata/main/pilot6July2023/expired0.1XSGD.json',
+  );
+  console.log('PBM Token type 3 created');
+  await new Promise((r) => setTimeout(r, 5000)); // UNCOMMENT to prevent rpc rate limiting if you are on free version
+
+
+  // creating token id 4: DSGD pbm - 1 DSGD
   await pbm.createPBMTokenType(
     'Grab1DSGD',
-    ethers.utils.parseUnits('1', 6),
+    ethers.utils.parseUnits('1', await dsgd.decimals()),
     'DSGD',
     expiryDate,
     deployer,
     'https://raw.githubusercontent.com/StraitsX/NFT-Metadata/main/pilot6July2023/1DSGD.json',
     'https://raw.githubusercontent.com/StraitsX/NFT-Metadata/main/pilot6July2023/expired1DSGD.json',
   );
-  console.log('PBM Token type 3 created');
+  console.log('PBM Token type 4 created');
   await new Promise((r) => setTimeout(r, 5000)); // UNCOMMENT to prevent rpc rate limiting if you are on free version
 
   // test expired token needs to change the expire time on the fly and remember to update metadata as well
--- Pilot6July2023/scripts/mint_pbm_tokens.js
+++ Pilot6July2023/scripts/mint_pbm_tokens.js
@@ -10,8 +10,14 @@ async function main() {
     .connect(deployerSigner);
 
   // xsgd on mumbai: 0x16e28369bc318636abbf6cb1035da77ffbf4a3bc
-  const xsgdAddress = "0x787bD10Bb65AE206f70759D88a2ffc0F2653C0F6"
-  const dsgdAddress = "0xd769410dc8772695A7f55a304d2125320A65c2a5"
+  // fake stablecoin addresses on mainnet
+  // const xsgdAddress = '0x787bD10Bb65AE206f70759D88a2ffc0F2653C0F6';
+  // const dsgdAddress = '0xd769410dc8772695A7f55a304d2125320A65c2a5';
+
+  // mumbai xsgd and 2 decimals dsgd addresses
+  const xsgdAddress = '0x16e28369bc318636abbf6cb1035da77ffbf4a3bc';
+  const dsgdAddress = '0xD2a6a1F2954a04EF90152da0fC9E15b5c68E7A69';
+
   const XSGD = await ethers.getContractFactory('Spot');
   const xsgd = await XSGD.attach(xsgdAddress).connect(deployerSigner); // XSGD deployed on mumbai
 
@@ -24,21 +30,26 @@ async function main() {
   await dsgd.increaseAllowance(pbm.address, 10000000000000);
 
   const swapDeployment = await deployments.get('Swap');
-  const swap = (await ethers.getContractFactory('Swap')).attach(swapDeployment.address).connect(deployerSigner);
+  const swap = (await ethers.getContractFactory('Swap'))
+    .attach(swapDeployment.address)
+    .connect(deployerSigner);
 
-  // mint PBM token 0, 1, 2, 3
-  const tjAddr = "0x56285Cbc175a9c7eB347d95a15633D95f894ba7b"
-  const victorAddr = "0x4Ef6462589a2D509fc05dA74511FFB4275D36615"
-  const grabAddr = ["0xf64b2f9577b09e83e23543208eaa8b1ec919458e", "0xb1fee2dbecd97132c401b0c068a69bb249423fb9"]
+  // mint PBM token 0 -> 1 xsgd, 1 -> 2 xsgd, 2 -> 5 xsgd, 3 -> 0.1 xsgd, 4 -> 1 dsgd
+  const tjAddr = '0x56285Cbc175a9c7eB347d95a15633D95f894ba7b';
+  const victorAddr = '0x4Ef6462589a2D509fc05dA74511FFB4275D36615';
+  const grabAddr = [
+    '0xf64b2f9577b09e83e23543208eaa8b1ec919458e',
+    '0xb1fee2dbecd97132c401b0c068a69bb249423fb9',
+  ];
 
   // mint dummy XSGD
   // await xsgd.mint(swap.address, ethers.utils.parseUnits('1000000', 6))
   // await xsgd.mint(victorAddr, ethers.utils.parseUnits('1000000', 6))
 
-  const mintTo = [victorAddr]
+  const mintTo = [victorAddr];
   for (let i = 0; i < mintTo.length; i++) {
-    await pbm.batchMint([0, 1, 2, 3], [5000, 5000, 5000, 5000], mintTo[i]);
-    console.log(`minted PBM token 0, 1, 2, 3 to address ${mintTo[i]}`);
+    await pbm.batchMint([0, 1, 2, 3, 4], [5000, 5000, 5000, 5000, 5000], mintTo[i]);
+    console.log(`minted PBM token 0, 1, 2, 3, 4 to address ${mintTo[i]}`);
     await new Promise((r) => setTimeout(r, 5000));
   }
 }
