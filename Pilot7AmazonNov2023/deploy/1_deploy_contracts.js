d92e9fe48284a578185536eec72dc59beedfeb76
Jacob <jacobshan429@gmail.com>
2023-09-08 03:51:33 +0800

    chore: mumbai deployment

--- Pilot7AmazonNov2023/deploy/1_deploy_contracts.js
+++ Pilot7AmazonNov2023/deploy/1_deploy_contracts.js
@@ -1,11 +1,6 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy('PBMAddressList', {
-    from: deployer,
-    args: [],
-    log: true,
-  });
   await deploy('PBM', {
     from: deployer,
     args: [],
