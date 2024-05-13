8c477001914333e1b931bf803b4a3012bba0cd6a
Victor Liew <1174679+alcedo@users.noreply.github.com>
2024-05-13 23:53:38 +0800

    added some test cases for pbmPaymentTest

--- NoahPBM/test/paymentpbm/pbmPayment.test.js
+++ NoahPBM/test/paymentpbm/pbmPayment.test.js
@@ -7,8 +7,9 @@ async function init() {
   let xsgdToken = await deploy('Spot', 'XSGD', 'XSGD', 6);
   let noahPaymentManager = await deploy('NoahPaymentManager');
   let pbm = await deploy('PBMPayment');
+  let addressList = await deploy('PBMMerchantAddressList');
 
-  return [xsgdToken, noahPaymentManager, pbm]
+  return [xsgdToken, noahPaymentManager, pbm, addressList]
 }
 
 describe('PBM PaymentTest', async () => {
@@ -25,13 +26,21 @@ describe('PBM PaymentTest', async () => {
   let xsgdToken;
   let noahPaymentManager;
   let pbm; 
+  let addressList; 
 
   beforeEach(async () => {
-    [xsgdToken, noahPaymentManager, pbm] = await init();
+    [xsgdToken, noahPaymentManager, pbm, addressList] = await init();
     
-    // Init accouns with spot tokens
+    // Init accts with spot tokens
     await xsgdToken.mint(accounts[0].address,parseUnits('10000', await xsgdToken.decimals()));
     await xsgdToken.mint(accounts[1].address,parseUnits('10000', await xsgdToken.decimals()));
+
+    await pbm.initialise(
+      Math.round(new Date().getTime() / 1000 + 86400 * 30), 
+      addressList.address,
+      noahPaymentManager.address
+    )
+
   });
 
   /** Verify Deployments first */
@@ -51,112 +60,47 @@ describe('PBM PaymentTest', async () => {
   });
   
 
-  /** Noah Payment related tests*/
-  describe('Noah PBM Core Test', async () => {
-
-    
-    it('Should ensure ', async () => {});
-    
-    it('Should ensure basic deposit check - ERC20 balance and treasury balance updated upon deposit of funds for a PBM', async () => {
-        let funderSigner = accounts[1];
-
-        // set approval to pull funds 
-        let xsgdContractUser = xsgdToken.connect(funderSigner);
-        await xsgdContractUser.increaseAllowance(
-          noahPaymentManager.address,
-          parseUnits('500', await xsgdToken.decimals())
-        );
-        expect(await xsgdToken.balanceOf(funderSigner.address)).to.equals(10000000000)
-
-        // mint and pull funds 
-        await noahPaymentManager.connect(funderSigner).depositForPBMAddress(
-          pbm.address,
-          xsgdToken.address,
-          parseUnits('500', await xsgdToken.decimals())
-        );
-        
-        // check balance transfered from funder to noah payment manager on behalf of PBM
-        let tokenBalance = await noahPaymentManager.getPBMCampaignTokenBalance(pbm.address, xsgdToken.address);
-        expect(tokenBalance).to.equal(parseUnits('500', await xsgdToken.decimals()));
-        console.log(await xsgdToken.balanceOf(funderSigner.address));
-        expect(await xsgdToken.balanceOf(funderSigner.address)).to.equals(9500000000);
-
-    });
-    
-    it('Should ensure crawler role is being defined', async () => {
-      expect(await noahPaymentManager.NOAH_CRAWLER_ROLE()).equals(
-        "0x1fd1b424520c6953ed4b151586253c6b4fe3183d39b856d80f513a00f543a978");
-
-    });
-
-
-    it('Should ensure that only campaign PBM can spend its own money', async () => {});
-    
-    // required to ensure admin roles are not hijacked
-    it('Should ensure that only Noah PBM can only be init once', async () => {});
-    it('Should ensure payments can only be created if enough funding', async () => {});
-
-    it('Should ensure ERC20 balance and treasury balance are NOT changed after payment Created', async () => {});
-    it('Should ensure ERC20 balance and treasury balance are NOT changed after payment Cancelled', async () => {});
-
-    it('Should ensure ERC20 balance and treasury balance are updated after payment COMPLETED', async () => {});
-    it('Should ensure ERC20 balance and treasury balance are updated after payment Refunded', async () => {});
-
-    it('Should ensure ERC20 balance and treasury balance are updated after payment Refunded', async () => {});
-
-
-    describe('Treasury Balance Test', async () => {
-
-      it('Should ensure all treasury related functions are STRICTLY internal only functions', async () => {});
-
-      it('Should ensure Owner is able to recover erc20 tokens in this smart contract', async () => {});
-
-      it('Should ensure _increaseTrasuryBalance only increase main balance', async () => {});
-      it('Should ensure _markCompleteTreasuryBalanace only decrease pending balance', async () => {});
-      it('Should ensure _markPendingTreasuryBalanace moves from main to pending balance', async () => {});
-      it('Should ensure _revertPendingTreasuryBalanace moves from pending to main balance', async () => {});
-
-    });
-
-
-    describe('Payment Created Test', async () => {
-
-      it('Should ensure pbm balance is moved to pending balance upon PaymentCreated event', async () => {});
-
-    });
-
-    describe('Payment Completed Test', async () => {
-
-      it('Should ensure erc20 token is disburse to destination', async () => {});
-      it('Should ensure internal pending balance is updated only', async () => {});
-
-    });
-
-    describe('Payment Cancel Test', async () => {
-
-      it('Should ensure pbm pending balance is moved back to pbm balance upon PaymentCreated event', async () => {});
-
-
-    });
-    
-    
-
-  });
-
-
-
   /** PBM Payment related & end to end tests */
   describe('PBM Payment Core Test', async () => {
-    
 
-    it('Should have initialised done properly', async () => {
+    it('Should have been initialised properly', async () => {
+        assert(await pbm.pbmAddressList() != '');
+        assert(await pbm.pbmTokenManager() != '');
+        assert(await pbm.noahPaymentManager() != '');
+        assert(await pbm.contractExpiry() != 0);
+    });
 
+    describe('PBM Minting Test', async () => {
+
+      it('Only owner should be able to mint unbacked PBM tokens', async () => {
+      });
+
+      it('PBM displays the correct token information', async () => {});
+      it('PBM displays the correct token balance details', async () => {});
+
+      it('PBM ', async () => {});
+      it('PBM ', async () => {});
 
     });
 
+    // Payment test
+      // only merchant can be paid to
+      // direct payment 
+        // balance is correct
+      // indirect payment
+        // balance is correct
+      // events tests - correct events are emitted
+     
+    // P2P Transfer test 
+      // p2p transfer works 
 
-    it('Only owner should be able to mint unbacked PBM tokens', async () => {
-    });
+    // Tear down tests
+      // Revoke
+      // Recovery of erc20 token tests
+
+
+    // Expiry date tests
+
     it('PBM should call swap on NoahPBM if inadequate payment currency', async () => {});
     it('PBM should be able to combine various PBM types in accordance to combination logic', async () => {});
 
