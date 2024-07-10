b2439df2322b6cc375158e79cd3f09f126f3b79f
Victor Liew <1174679+alcedo@users.noreply.github.com>
2024-07-11 01:36:56 +0800

    added orchid testers list

--- NoahPBM/scripts/mintPBMPaymentToken.js
+++ NoahPBM/scripts/mintPBMPaymentToken.js
@@ -19,17 +19,32 @@ async function main() {
   const XSGD = await ethers.getContractFactory("Spot");
   const xsgd = await XSGD.attach(spotAddress).connect(deployerSigner);
 
-  // How much to mint
-  const DOLLAR_VALUE_TO_MINT = "10"; // 0.1 stands for 10 cents, 1.0 stands for 1 dollar
 
   const receiverList = [
     // "0x2E78aF1d35644fedaeaaf8CA8ACb70D0B35d3b12", // Victor AVAX SAFE-WALLET
-    // "0xF642be06350DAe9dD475E5A5ad6e25038F295B28", // Victor AVAX EOA Wallet
+    "0xF642be06350DAe9dD475E5A5ad6e25038F295B28", // Victor AVAX EOA Wallet
     // "0x4F67e8Df00d66C36a6d63afC62A29058eC03551A", // Advait AVAX Safe-wallet
-    "0x57358A3280603F8863e0006c48c8Ba869fd85D83", // Tianwei avax safe-wallet
+    // "0x77018cbA4eD6706028B679c47A44290d47f6B7D2", // Wong tse jian safe wallet 
   ];
 
-  for(const receiver of receiverList) {
+  const orchidHeroTester = [
+    "0x992b89D8d303A41e66A04B192c529c46e5d4E367", // Venice
+    "0x11e3dEdAd810e5a26BfC6677cFF858aA3957A2e8", // Jodie
+    "0x27e923873c89fa7aa60988Dbdc8c806B309900Ab", // Greg
+    "0xC7A7359F54Bd7E4E22497d08A4A57e56214f4e88", // kah hong
+    "0xf1533290DBE91416BcFaef3dB7E5Aa7A76156614", // jing hang
+    "0x37932e0c68340b10980029f184A1a06aF1476b6d", // nicky tan
+    "0x9fB1D1bC972c095769E2b815BD1a59117a8DdE82", // yik
+    "0x978e4ee96434F1F4943cb51c794Bd1Cf82ba6f4A", // zack
+    "0x04358464Ebfb7E6EB0D1a46bD25919210A50f546", // yanyi
+    "0xd68bF6851202027502C71BB15d97a345D0f9EA62", // alvin
+    // "0x57358A3280603F8863e0006c48c8Ba869fd85D83", // Tianwei avax safe-wallet
+  ];
+
+  // How much to mint
+  const DOLLAR_VALUE_TO_MINT = "1"; // 0.1 stands for 10 cents, 1.0 stands for 1 dollar
+
+  for(const receiver of orchidHeroTester) {
 
     // increase allowance for PBM
     await xsgd.increaseAllowance(pbmPayment.address, ethers.utils.parseUnits(DOLLAR_VALUE_TO_MINT, await xsgd.decimals()));
