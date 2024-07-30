86ccb5e0c0cb0cbadcc707fd3dd08b307602f8ac
Jacob <jacobshan429@gmail.com>
2024-07-30 16:35:54 +0800

    chore: format

--- NoahPBM/scripts/currencyPBMInit.js
+++ NoahPBM/scripts/currencyPBMInit.js
@@ -1,32 +1,34 @@
 const { ethers, getNamedAccounts } = require("hardhat");
 async function main() {
-    const { deployer } = await getNamedAccounts();
-    const deployerSigner = ethers.provider.getSigner(deployer);
+  const { deployer } = await getNamedAccounts();
+  const deployerSigner = ethers.provider.getSigner(deployer);
 
-    const noahPaymentManagerAddr = "0x5201D8EF22bA3090eA3329d57F082801783A8558"
-    const noahPaymentManager = (await ethers.getContractFactory("NoahPaymentManager")).attach(noahPaymentManagerAddr).connect(deployerSigner);
+  const noahPaymentManagerAddr = "0x5201D8EF22bA3090eA3329d57F082801783A8558";
+  const noahPaymentManager = (await ethers.getContractFactory("NoahPaymentManager"))
+    .attach(noahPaymentManagerAddr)
+    .connect(deployerSigner);
 
-    console.log("NoahPaymentManager address", noahPaymentManagerAddr);
+  console.log("NoahPaymentManager address", noahPaymentManagerAddr);
 
-    await noahPaymentManager.initialise();
-    console.log("NoahPaymentManager initialised");
+  await noahPaymentManager.initialise();
+  console.log("NoahPaymentManager initialised");
 
-    const currencyPBMAddr = "";
-    const currencyPBM = (await ethers.getContractFactory("CurrencyPBM")).attach(currencyPBMAddr).connect(deployerSigner);
+  const currencyPBMAddr = "";
+  const currencyPBM = (await ethers.getContractFactory("CurrencyPBM")).attach(currencyPBMAddr).connect(deployerSigner);
 
-    console.log("currencyPBM address", currencyPBMAddr);
+  console.log("currencyPBM address", currencyPBMAddr);
 
-    const addressListAddr = "0x642Fa8F823FF2A6982048F1A4dD0e80DB01E3B78";
-    console.log("PBMMerchantAddressList address", addressListAddr);
+  const addressListAddr = "0x642Fa8F823FF2A6982048F1A4dD0e80DB01E3B78";
+  console.log("PBMMerchantAddressList address", addressListAddr);
 
-    const expiryDate = 2713795200; // December 31, 2055 12:00:00 AM GMT+08:00
+  const expiryDate = 2713795200; // December 31, 2055 12:00:00 AM GMT+08:00
 
-    await currencyPBM.initialise(expiryDate, addressListAddr, noahPaymentManagerAddr);
-    console.log("currencyPBM initialised");
+  await currencyPBM.initialise(expiryDate, addressListAddr, noahPaymentManagerAddr);
+  console.log("currencyPBM initialised");
 }
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
